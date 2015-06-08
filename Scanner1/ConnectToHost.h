31212efcdb313d8346ced8279c6fd64acb550d9e
Jess-Yuan <zhuohao.yuen@gmail.com>
2015-06-08 22:46:38 +0800

    Scanner

---
+++ Scanner1/ConnectToHost.h
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+#pragma comment(lib, "ws2_32.lib")
+#include <WinSock2.h>
+
+using namespace std;
+
+SOCKET s; //Socket handle
+
+//CONNECTTOHOST ¨C Connects to a remote host
+bool ConnectToHost(int PortNo, const char* IPAddress)
+{
+	//Start up Winsock¡­
+	WSADATA wsadata;
+
+	int error = WSAStartup(MAKEWORD(2, 2), &wsadata);
+
+	//Did something happen?
+	if (error)
+		return false;
+	//Did we get the right Winsock version?
+	//if (wsadata.wVersion != 2)
+	//{
+	//	WSACleanup(); //Clean up Winsock
+	//	return false;
+	//}
+	//Fill out the information needed to initialize a socket¡­
+	SOCKADDR_IN target; //Socket address information
+
+	target.sin_family = AF_INET; // address family Internet
+	target.sin_port = htons(PortNo); //Port to connect on
+	target.sin_addr.S_un.S_addr = inet_addr("127.0.0.1"); //Target IP
+
+	s = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP); //Create socket
+	if (s == INVALID_SOCKET)
+	{
+		return false; //Couldn't create the socket
+	}
+
+	//Try connecting...
+
+	if (connect(s, (SOCKADDR *)&target, sizeof(target)) == SOCKET_ERROR)
+	{
+		return false; //Couldn't connect
+	}
+	else
+		return true; //Success
+}
+//CLOSECONNECTION ¨C shuts down the socket and closes any connection on it
+void CloseConnection()
+{
+	//Close the socket if it exists
+	if (s)
+		closesocket(s);
+
+	WSACleanup(); //Clean up Winsock
+}
---
+++ Scanner1/Scanner.cpp
@@ -0,0 +1,13 @@
+#include "Scanner.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+int main(array<String^>^ args)
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+
+	Scanner1::Scanner form;
+	Application::Run(%form);
+}
